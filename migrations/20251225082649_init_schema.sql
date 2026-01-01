be9d4f4d444ade710538727cdfdca155dcb73e5c
Evgeny Gurov <evgenygurov9@mail.ru>
2026-01-01 20:45:10 +0300

    [migrations] fix sql migration

--- migrations/20251225082649_init_schema.sql
+++ migrations/20251225082649_init_schema.sql
@@ -2,17 +2,9 @@
 -- +goose StatementBegin
 create schema loyalty;
 
-alter database dev
-set
-    time zone 'Europe/Moscow';
-
 -- +goose StatementEnd
 -- +goose Down
 -- +goose StatementBegin
 drop schema loyalty cascade;
-
-alter database dev
-set
-    time zone 'UTC';
 
 -- +goose StatementEnd
